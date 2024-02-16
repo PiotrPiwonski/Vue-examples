9f9285a8af5c20c04bb3071b3ce4fdb46165c977
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-02-16 16:20:24 +0100

    use reactive example

--- src/components/MyAwsomeComponet.vue
+++ src/components/MyAwsomeComponet.vue
@@ -1,24 +1,39 @@
 <template>
   <div>
- <button v-on:click="incrementPension">Increment</button>
- <button @click="sayHello">Say hello</button>
-    <p>Your pension: {{pension}}</p>
+    <h1>{{price}}$</h1>
+    <button @click="makeOrder">Make naext order</button>
+    <p>Quantity: {{quantity}}</p>
+    <p>Order total price: {{totalPrice}}</p>
+    <p>Tax: {{tax}}</p>
   </div>
 </template>
 
 <script>
-import {ref} from "vue";
+import { reactive, toRefs } from "vue";
 
 export default {
   name: 'MyAwsomeComponet',
   setup() {
-    const pension = ref(0);
 
-    function incrementPension() {
-      pension.value++;
+    // const quantity = ref(0);
+    // const price = ref(100);
+    // const totalPrice = ref(0);
+    // const tax = ref(0);
+
+    const state = reactive({
+      quantity: 0,
+      price: 100,
+      totalPrice: 0,
+      tax: 0
+    })
+
+    function makeOrder() {
+      state.quantity++;
+      state.totalPrice = state.quantity * state.price;
+      state.tax = state.totalPrice * 0.23;
     }
 
-    return { pension, incrementPension };
+    return { makeOrder, ...toRefs(state) };
   }
 }
 </script>
