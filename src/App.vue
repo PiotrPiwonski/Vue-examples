1a4f91282f270e80751a95adb10e7ec199813744
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-02-19 13:53:28 +0100

    use Lifecycle hooks examples

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <siemaApp/>
   <MyAwsomeComponet/>
 </template>
 
 <script>
 import MyAwsomeComponet from './components/MyAwsomeComponet.vue';
-import siemaApp from "@/components/siemaApp.vue";
 
 export default {
   name: 'App',
   components: {
     MyAwsomeComponet,
-    siemaApp
   }
 }
 </script>
---
+++ src/components/MyAwesomeComponentChild.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <p>Hi, I'm Child Component</p>
+    <p>Window size: Width: {{width}} Height: {{height}}</p>
+  </div>
+</template>
+
+<script>
+
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref
+} from "vue";
+
+export default {
+  name: 'MyAwsomeComponentChild',
+  setup() {
+    console.log('Child setup');
+
+    const width = ref(0);
+    const height = ref(0);
+
+    function changeWindowSize() {
+      width.value = window.innerWidth;
+      height.value = window.innerHeight;
+    }
+
+    onBeforeMount(() => {
+      console.log('Child onBeforeMount');
+    });
+    onMounted(() => {
+      console.log('Child onMounted');
+      window.addEventListener('resize', changeWindowSize)
+    });
+    onBeforeUpdate(() => {
+      console.log('Child onBeforeUpdate');
+    });
+    onUpdated(() => {
+      console.log('Child onUpdated');
+    });
+    onBeforeUnmount(() => {
+      console.log('Child onBeforeUnmount');
+      window.removeEventListener('resize', changeWindowSize);
+    });
+    onUnmounted(() => {
+      console.log('Child onUnmounted');
+    });
+
+
+    return {height, width}
+  }
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
