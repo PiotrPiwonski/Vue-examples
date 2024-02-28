cb6ef48d07268315844dbd5ccab5b361ecf72b6d
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-02-28 13:33:36 +0100

    use vue router examples

---
+++ src/views/ContactView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Contact page</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'ContactView',
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
