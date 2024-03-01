82ad38eaee83c1f1d65ba2697e370dc4e3c0c2ff
PiotrPiwonski <49531459+PiotrPiwonski@users.noreply.github.com>
2024-03-01 12:13:44 +0100

    use handling a non existent resource examples

--- src/components/ItemDetails.vue
+++ src/components/ItemDetails.vue
@@ -6,6 +6,13 @@
     <p>Height: {{item.height}}</p>
     <p>Mass: {{item.mass}}</p>
   </section>
+  <section v-else class="not-found-page">
+    <h2 class="not-found-page__header">OOOPS!</h2>
+    <p>
+      For some reason the resorce you looking for does not exist or is unavailable.
+      Please try something else.
+    </p>
+  </section>
 </template>
 
 <script>
@@ -22,11 +29,18 @@ export default {
 
   async setup(props) {
     const item = ref(null);
-    const response = await fetch(`https://swapi.dev/api/people/${props.id}`);
-    const parseResponse = await response.json();
+    
+    try {
+      const response = await fetch(`https://swapi.dev/api/people/${props.id}`);
+      if (response.status !== 200) {
+        throw new Error('Response status is different than 200.')
+      }
+      const parseResponse = await response.json();
+      item.value = parseResponse;
+    } catch (error) {
+      console.log(error);
+    }
 
-    item.value = parseResponse;
-    console.log(item);
     return {
       item
     }
@@ -34,4 +48,12 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.not-found-page {
+  text-align: center;
+
+  &__header {
+    color: #fcd711;
+  }
+}
+</style>
